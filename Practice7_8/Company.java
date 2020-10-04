c1ef00e0c071250474185195408056db1cd0ec8f
DanilaShikalov <dan.shikalov@gmail.com>
2020-10-04 23:59:35 +0300

    1

--- Practice7_8/Company.java
+++ Practice7_8/Company.java
@@ -4,24 +4,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Company {
+    private ArrayList<Employee> Listt = new ArrayList<>();
     private double Summa = 0;
-    private ArrayList<Employee> employeeList = new ArrayList<>();
 
 
     public void hire(Employee employee) {
-        employeeList.add(employee);
+        Listt.add(employee);
     }
 
 
     public void fire(int i) {
-        employeeList.remove(i);
+        Listt.remove(i);
         getIncome();
     }
 
 
     public double getIncome() {
         this.Summa = 0;
-        for (Employee emp : employeeList) {
+        for (Employee emp : Listt) {
             if (emp.GetWorkPlace() instanceof Manager) {
                 Summa = Summa + ((Manager) emp.GetWorkPlace()).Polza();
             }
@@ -30,7 +30,7 @@ public class Company {
     }
 
     List<Employee> getTopSalaryStaff(int count) {
-        ArrayList<Employee> employees = employeeList;
+        ArrayList<Employee> employees = Listt;
         Employee t;
         for (int i = 0; i < employees.size() - 1; i++)
             for (int j = i + 1; j < employees.size(); j++) {
@@ -44,7 +44,7 @@ public class Company {
     }
 
     List<Employee> getLowestSalaryStaff(int count) {
-        ArrayList<Employee> employees = employeeList;
+        ArrayList<Employee> employees = Listt;
         Employee t;
         for (int i = 0; i < employees.size() - 1; i++)
             for (int j = i + 1; j < employees.size(); j++) {
@@ -58,14 +58,14 @@ public class Company {
     }
 
     public int getListSize() {
-        return employeeList.size();
+        return Listt.size();
     }
 
     public void hireAll(ArrayList<Employee> listEmployee) {
-        employeeList.addAll(listEmployee);
+        Listt.addAll(listEmployee);
     }
 
     public void fire(Employee employee) {
-        employeeList.remove(employee);
+        Listt.remove(employee);
     }
 }
